6cc1072858dd785b3f23ed531e3e222016a33a8c
mr-phlames <mickdd22@gmail.com>
2020-07-07 05:47:03 +0000

    added serve command

---
+++ src/App/ServeCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Leaf\Console\App;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ServeCommand extends Command
+{
+	protected static $defaultName = 'app:serve';
+
+	protected function configure()
+	{
+		$this
+			->setHelp("Start the leaf app server")
+			->setDescription("Run your Leaf app")
+			->addOption('port', 'p', InputOption::VALUE_NONE, 'Port to run app on');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$port = $input->getOption("port") ? (int) $input->getOption("port") : 5500;
+
+		$output->writeln("<info>Leaf development server started: http://localhost:$port</info>");
+		$output->writeln("<comment>Happy coding!</comment>");
+		$output->write(shell_exec("php -S localhost:{$port}"));
+	}
+}
