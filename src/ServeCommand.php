2d1a90888a514d53e652bc05401aacb6ae49cc4f
Michael Darko <mickdd22@gmail.com>
2021-11-17 18:47:40 +0000

    :sparkles: added proper error handling to server

--- src/ServeCommand.php
+++ src/ServeCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
 
 class ServeCommand extends Command
 {
@@ -22,9 +23,16 @@ class ServeCommand extends Command
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		$port = $input->getOption("port") ? (int) $input->getOption("port") : 5500;
+		$process = Process::fromShellCommandline("php -S localhost:$port", null, null, null, null);
 
-		$output->writeln("<info>Leaf development server started: http://localhost:$port</info>");
-		$output->writeln("<comment>Happy coding!</comment>");
-		$output->write(shell_exec("php -S localhost:{$port}"));
+		return $process->run(function ($type, $line) use ($output, $port) {
+			if (is_string($line) && !strpos($line, 'Failed')) {
+				$output->writeln("<info>Leaf development server started: http://localhost:$port</info>");
+				$output->writeln("<comment>Happy coding!</comment>");
+				$output->write($line);
+			} else {
+				$output->write("<error>$line</error>");
+			}
+		});
 	}
 }
