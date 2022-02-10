53fd8d76d1938f775e2d2ba59fd4450e5ed71b83
Michael Darko <mickdd22@gmail.com>
2022-02-10 22:54:05 +0000

    :sparkles: added dependency install & updates

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -15,35 +15,87 @@ class InstallCommand extends Command
 	protected function configure()
 	{
 		$this
-			->setHelp("Install a new package")
-			->setDescription("Add a new package to your leaf app")
-			->addArgument("package", InputArgument::REQUIRED, "package to install")
-			->addArgument("version", InputArgument::OPTIONAL, "version to install");
+			->setHelp('Install a new package')
+			->setDescription('Add a new package to your leaf app')
+			->addArgument('packages', InputArgument::IS_ARRAY, 'package(s) to install. Can also include a version constraint, e.g. foo/bar or foo/bar@1.0.0');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-		$package = $input->getArgument("package");
-		if (strpos($package, '/') == false) $package = "leafs/$package";
+		$packages = $input->getArgument('packages');
 
-		$version = $input->getArgument("version") ?? null;
+		if (count($packages)) {
+			return $this->install($packages, $output);
+		}
 
-		$output->writeln("<info>Installing $package...</info>");
+		return $this->installDependencies($output);
+	}
+
+	protected function installDependencies($output)
+	{
+		$composerJsonPath = getcwd() . '/composer.json';
+		$composerLockPath = getcwd() . '/composer.lock';
+
+		if (!file_exists($composerJsonPath)) {
+			$output->writeln('<error>No composer.json found in the current directory. Pass in a package to add if you meant to install something.</error>');
+			return 1;
+		}
+
 		$composer = $this->findComposer();
 		$process = Process::fromShellCommandline(
-			"$composer require $package $version", null, null, null, null
+			file_exists($composerLockPath) ? "$composer install" : "$composer update",
+			null,
+			null,
+			null,
+			null
 		);
 
 		$process->run(function ($type, $line) use ($output) {
 			$output->write($line);
 		});
 
-		if ($process->isSuccessful()) {
+		if (!$process->isSuccessful()) return 1;
+
+		$output->writeln('<comment>packages installed successfully!</comment>');
+
+		return 0;
+	}
+
+	/**
+	 * Install packages
+	 */
+	protected function install($packages, $output)
+	{
+		foreach ($packages as $package) {
+			if (strpos($package, '/') == false || strpos($package, '.') == false) {
+				$package = "leafs/$package";
+			}
+			$package = str_replace('@', ':', $package);
+
+			$output->writeln("<info>Installing $package...</info>");
+			$composer = $this->findComposer();
+			$process = Process::fromShellCommandline(
+				"$composer require $package",
+				null,
+				null,
+				null,
+				null
+			);
+
+			$process->run(function ($type, $line) use ($output) {
+				$output->write($line);
+			});
+
+			if (!$process->isSuccessful()) return 1;
+
 			$output->writeln("<comment>$package installed successfully!</comment>");
-			return 0;
 		}
 
-		return 1;
+		if (count($packages) > 1) {
+			$output->writeln('<info>All packages installed</info>');
+		}
+
+		return 0;
 	}
 
 	/**
