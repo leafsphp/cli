2430a7710f97e25f5630683667e5ed53c69ffa0b
Michael Darko <mickdd22@gmail.com>
2022-02-23 11:37:51 +0000

    :recycle: swicthed to single quotes

--- src/InteractCommand.php
+++ src/InteractCommand.php
@@ -8,24 +8,24 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Psy\Shell;
 
 class InteractCommand extends Command {
-    protected static $defaultName = "interact";
+    protected static $defaultName = 'interact';
 
     protected function configure()  {
         $this
-            ->setDescription("Interact with your application")
-            ->setHelp("Interact with your application");
+            ->setDescription('Interact with your application')
+            ->setHelp('Interact with your application');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-        $output->writeln("<info>Leaf interactive shell activated</info>");
+        $output->writeln('<info>Leaf interactive shell activated</info>');
 
-        if (file_exists("vendor/autoload.php")) require "vendor/autoload.php";
-        if (file_exists("Config/bootstrap.php")) require "Config/bootstrap.php";
-        if (file_exists("index.php") && !file_exists("leaf")) require "index.php";
+        if (file_exists('vendor/autoload.php')) require 'vendor/autoload.php';
+        if (file_exists('Config/bootstrap.php')) require 'Config/bootstrap.php';
+        if (file_exists('index.php') && !file_exists('leaf')) require 'index.php';
 
-        if (!file_exists("vendor/autoload.php") && !file_exists("Config/bootstrap.php") && (file_exists("index.php") && file_exists("leaf"))) {
-            $output->writeln("<info>Required files not found, starting shell running in retard mode...</info>");
+        if (!file_exists('vendor/autoload.php') && !file_exists('Config/bootstrap.php') && (file_exists('index.php') && file_exists('leaf'))) {
+            $output->writeln('<info>Required files not found, starting shell running in retard mode...</info>');
         }
 
         $shell = new Shell();
