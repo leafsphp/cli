f91a84a55ad1fbcb454e71edb762382112437900
Michael Darko <mickdd22@gmail.com>
2022-02-23 11:37:00 +0000

    :recycle: swicthed to single quotes

--- src/ServeCommand.php
+++ src/ServeCommand.php
@@ -15,20 +15,20 @@ class ServeCommand extends Command
 	protected function configure()
 	{
 		$this
-			->setHelp("Start the leaf app server")
-			->setDescription("Run your Leaf app")
+			->setHelp('Start the leaf app server')
+			->setDescription('Run your Leaf app')
 			->addOption('port', 'p', InputOption::VALUE_OPTIONAL, 'Port to run app on');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-		$port = $input->getOption("port") ? (int) $input->getOption("port") : 5500;
+		$port = $input->getOption('port') ? (int) $input->getOption('port') : 5500;
 		$process = Process::fromShellCommandline("php -S localhost:$port", null, null, null, null);
 
 		return $process->run(function ($type, $line) use ($output, $port) {
 			if (is_string($line) && !strpos($line, 'Failed')) {
 				$output->writeln("<info>Leaf development server started: http://localhost:$port</info>");
-				$output->writeln("<comment>Happy coding!</comment>");
+				$output->writeln('<comment>Happy coding!</comment>');
 				$output->write($line);
 			} else {
 				$output->write("<error>$line</error>");
