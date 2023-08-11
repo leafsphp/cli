4079f5ac16f7746bc6f08bff0fe30c924fbff063
mychidarko <mickdd22@gmail.com>
2023-08-11 00:51:32 +0000

    feat: install deps before run

--- src/ViewDevCommand.php
+++ src/ViewDevCommand.php
@@ -11,23 +11,35 @@ use Symfony\Component\Process\Process;
 
 class ViewDevCommand extends Command
 {
-	protected static $defaultName = 'view:dev';
+    protected static $defaultName = 'view:dev';
 
-	protected function configure()
-	{
-		$this
+    protected function configure()
+    {
+        $this
             ->setAliases(['view:serve', 'view:watch'])
-			->setHelp('Run your frontend dev command')
-			->setDescription('Run your frontend dev server');
-	}
+            ->setHelp('Run your frontend dev command')
+            ->setDescription('Run your frontend dev server');
+    }
 
-	protected function execute(InputInterface $input, OutputInterface $output): int
-	{
-		$npm = Utils\Core::findNpm();
-		$success = Utils\Core::run("$npm run dev", $output);
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $directory = getcwd();
+        $npm = Utils\Core::findNpm();
 
-		if (!$success) return 1;
+        if (!is_dir("$directory/node_modules")) {
+            $output->writeln("<info>Installing dependencies...</info>");
+            $success = Utils\Core::run("$npm install", $output);
 
-		return 0;
-	}
+            if (!$success) {
+                $output->writeln("<error>❌  Failed to install dependencies.</error>");
+                return 1;
+            }
+        }
+
+        $success = Utils\Core::run("$npm run dev", $output);
+
+        if (!$success) return 1;
+
+        return 0;
+    }
 }
