a73f7a427c4487ac71686a9926c2aded20af8de6
mychidarko <mickdd22@gmail.com>
2023-08-11 14:28:26 +0000

    chore: remove unused code

--- src/ViewBuildCommand.php
+++ src/ViewBuildCommand.php
@@ -7,26 +7,37 @@ namespace Leaf\Console;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
 
 class ViewBuildCommand extends Command
 {
-	protected static $defaultName = 'view:build';
+    protected static $defaultName = 'view:build';
 
-	protected function configure()
-	{
-		$this
-			->setHelp('Run your frontend dev command')
-			->setDescription('Run your frontend dev server');
-	}
+    protected function configure()
+    {
+        $this
+            ->setHelp('Run your frontend dev command')
+            ->setDescription('Run your frontend dev server');
+    }
 
-	protected function execute(InputInterface $input, OutputInterface $output): int
-	{
-		$npm = Utils\Core::findNpm();
-		$success = Utils\Core::run("$npm run build", $output);
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $directory = getcwd();
+        $npm = Utils\Core::findNpm();
 
-		if (!$success) return 1;
+        if (!is_dir("$directory/node_modules")) {
+            $output->writeln("<info>Installing dependencies...</info>");
+            $success = Utils\Core::run("$npm install", $output);
 
-		return 0;
-	}
+            if (!$success) {
+                $output->writeln("<error>❌  Failed to install dependencies.</error>");
+                return 1;
+            }
+        }
+
+        $success = Utils\Core::run("$npm run build", $output);
+
+        if (!$success) return 1;
+
+        return 0;
+    }
 }
