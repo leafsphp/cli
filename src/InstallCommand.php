017245d7df9072943f8338af43d3283ccfe1d98b
mychidarko <mickdd22@gmail.com>
2024-10-30 10:09:12 +0000

    feat: add --dev option to install command

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -7,6 +7,7 @@ namespace Leaf\Console;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
@@ -19,7 +20,8 @@ class InstallCommand extends Command
 		$this
 			->setHelp('Install a new package')
 			->setDescription('Add a new package to your leaf app')
-			->addArgument('packages', InputArgument::IS_ARRAY, 'package(s) to install. Can also include a version constraint, e.g. foo/bar or foo/bar@1.0.0');
+			->addArgument('packages', InputArgument::IS_ARRAY, 'package(s) to install. Can also include a version constraint, e.g. foo/bar or foo/bar@1.0.0')
+            ->addOption('dev', 'd', InputOption::VALUE_NONE, 'Install package as a dev dependency');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
@@ -27,7 +29,7 @@ class InstallCommand extends Command
 		$packages = $input->getArgument('packages');
 
 		if (count($packages)) {
-			return $this->install($packages, $output);
+			return $this->install($packages, $input, $output);
 		}
 
 		return $this->installDependencies($output);
@@ -66,18 +68,19 @@ class InstallCommand extends Command
 	/**
 	 * Install packages
 	 */
-	protected function install($packages, $output)
+	protected function install($packages, $input, $output)
 	{
 		foreach ($packages as $package) {
 			if (strpos($package, '/') == false) {
 				$package = "leafs/$package";
 			}
+
 			$package = str_replace('@', ':', $package);
 
 			$output->writeln("<info>Installing $package...</info>");
 			$composer = Utils\Core::findComposer();
 			$process = Process::fromShellCommandline(
-				"$composer require $package",
+				"$composer require $package" . ($input->getOption('dev') ? ' --dev' : ''),
 				null,
 				null,
 				null,
