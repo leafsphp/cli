ec1de2d6631710500f8d16522b259746e8c7575c
mychidarko <mickdd22@gmail.com>
2025-01-25 19:50:35 +0000

    feat: switch tailwind install to v4

--- src/themes/tailwind/root/vite.config.js
+++ src/themes/tailwind/root/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import leaf from '@leafphp/vite-plugin';
+import { defineConfig } from "vite";
+import leaf from "@leafphp/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         leaf({
-            input: ['js/app.js', 'css/app.css'],
+            input: ["js/app.js", "css/app.css"],
             refresh: true,
         }),
     ],
