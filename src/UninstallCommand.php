c93fc46e8915fdbb30fad3cd67ed08003109f109
mychidarko <mickdd22@gmail.com>
2025-02-01 23:23:41 +0000

    chore: migrate uninstall command to sprout

--- src/UninstallCommand.php
+++ src/UninstallCommand.php
@@ -4,62 +4,34 @@ declare(strict_types=1);
 
 namespace Leaf\Console;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
+use Leaf\Sprout\Command;
 
 class UninstallCommand extends Command
 {
-    protected static $defaultName = 'uninstall';
+    protected $signature = 'uninstall {packages*}';
 
-    protected function configure()
+    protected $description = 'Uninstall a package';
+
+    protected function execute(): int
     {
-        $this
-            ->setHelp('The uninstall command removes a package from the current
-  list of installed packages')
-            ->setDescription('Uninstall a  package')
-            ->addArgument('packages', InputArgument::IS_ARRAY, 'package(s) to uninstall.');
-    }
+        $packages = $this->argument('packages');
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $packages = $input->getArgument('packages');
-
-        $composerJsonPath = getcwd() . '/composer.json';
-
-        if (!file_exists($composerJsonPath)) {
-            $output->writeln('<error>No composer.json found in the current directory.</error>');
-
+        if (!sprout()->composer()->json()) {
+            $this->writeln('<error>No composer.json found in the current directory.</error>');
             return 1;
         }
-
-        $composer = Utils\Core::findComposer();
 
         foreach ($packages as $package) {
             if (strpos($package, '/') == false) {
                 $package = "leafs/$package";
             }
 
-            $process = Process::fromShellCommandline(
-                "$composer remove $package",
-                null,
-                null,
-                null,
-                null
-            );
-
-            $process->run(function ($type, $line) use ($output) {
-                $output->write($line);
-            });
-
-            if (!$process->isSuccessful()) {
+            if (!sprout()->composer()->remove($package)->isSuccessful()) {
                 return 1;
             }
         }
 
-        $output->writeln('<comment>packages uninstalled successfully!</comment>');
+        $this->writeln('<comment>packages uninstalled successfully!</comment>');
 
         return 0;
     }
