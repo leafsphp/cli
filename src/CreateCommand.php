05a0c4776af1c48fe480d9431a13473b1acc9cf4
mychidarko <mickdd22@gmail.com>
2025-02-24 10:53:10 +0000

    fix: skip view setup on default selection

--- src/CreateCommand.php
+++ src/CreateCommand.php
@@ -204,7 +204,7 @@ class CreateCommand extends Command
 
             $extraCommands = ["cd '$directory'"];
 
-            if ($extraOptions['view'] ?? false && $extraOptions['view'] !== 'blade only') {
+            if (isset($extraOptions['view']) && $extraOptions['view'] !== 'blade only') {
                 $extraCommands[] = 'php leaf view:install --' . $extraOptions['view'];
             }
 
